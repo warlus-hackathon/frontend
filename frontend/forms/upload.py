32b7b41ecb415cc7039092b63cee3e8b217f8003
yankovskaya <yankovskaya.my@gmail.com>
2022-05-27 14:47:54 +0300

    feat: add upload form

---
+++ frontend/forms/upload.py
@@ -0,0 +1,24 @@
+import httpx
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
+from werkzeug.utils import secure_filename
+
+from frontend.config import config
+
+upload_url = f'{config.endpoint}/upload/'
+
+
+class UploadFileForm(FlaskForm):
+    file = FileField()
+
+    def upload_file(self) -> bool:
+        file = self.file.data
+        filename = secure_filename(file.filename)
+        files = {'file': (filename, file)}
+        timeout = httpx.Timeout(5.0, connect=5.0, read=50.0, write=5.0)
+        answer = httpx.post(upload_url, files=files, timeout=timeout)
+
+        if answer.status_code != '201':
+            return False
+
+        return True
