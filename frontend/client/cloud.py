0fc75db3949bc6df9097736753fe77f9874b4226
ProtKsen <protcenko.kseniya@gmail.com>
2022-05-28 20:46:27 +0500

    chore: change name for image storage

--- frontend/client/cloud.py
+++ frontend/client/cloud.py
@@ -7,11 +7,10 @@ from frontend.config import config
 class CloudClient:
 
     def download_input_by_name(self, filename: str) -> None:
-        upload_dir = Path('file_storage')
+        upload_dir = Path('frontend/static/images')
         upload_dir.mkdir(exist_ok=True, parents=True)
-        s3.download_file(config.aws.bucket_input_images, filename, f'file_storage/{filename}')
+        s3.download_file(config.aws.bucket_input_images, filename, f'{upload_dir}/{filename}')
 
     def download_output_by_name(self, filename: str) -> None:
-        upload_dir = Path('file_storage')
-        upload_dir.mkdir(exist_ok=True, parents=True)
-        s3.download_file(config.aws.bucket_input_images, filename, f'file_storage/{filename}')
+        upload_dir = Path('frontend/static/images')
+        s3.download_file(config.aws.bucket_output_images, filename, f'{upload_dir}/{filename}')
